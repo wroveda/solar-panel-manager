9836d3e63d6e2a746e2d788c0728287e33e34636
wroveda <walter.roveda@posteo.it>
2022-07-13 20:59:53 +0200

    Add info card system

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,42 @@
 <script setup>
+import wretch from "wretch";
+import { ref } from "vue";
+import InfoCard from "./components/InfoCard.vue";
+
+const panel = ref({});
+
+wretch()
+	.url("https://ott-fogliata.github.io/vuejs-s2i-repository/solar-panels.json")
+	.get()
+	.json(json => {panel.value = json})
 </script>
 
 <template>
+	<header>
+	</header>
+	<div class="info-container">
+		<InfoCard infoTitle="Power Production" :infoValue="panel['power-production'] + ' kW'" />
+		<InfoCard infoTitle="This Month's Energy" :infoValue="panel['month-energy'] + ' kWh'" />
+		<InfoCard infoTitle="Today's Energy" :infoValue="panel['today-energy'] + ' kWh'" />
+		<InfoCard infoTitle="Left Time Energy" :infoValue="panel['left-time-energy'] + ' kWh'" />
+	</div>
 </template>
 
 <style>
+* {
+	--clr-fg: #403F3B;
+	--clr-bg: #e4d9b4;
+	--clr-accent: #ffdd6e;
+	--clr-faded: #e4d295;
+}
+
+body {
+	color: var(--clr-fg);
+	background-color: var(--clr-bg);
+}
+
+.info-container {
+	display: flex;
+	width: 100%;
+}
 </style>
---
+++ src/components/InfoCard.vue
@@ -0,0 +1,33 @@
+<script setup>
+const props = defineProps({
+	infoTitle: String,
+	infoValue: String
+})
+</script>
+
+<template>
+	<div class="info">
+		<h3 class="info__title">{{ props.infoTitle }}</h3>
+		<p class="info__value">{{ props.infoValue }}</p>
+	</div>
+</template>
+
+<style scoped>
+.info {
+	background-color: var(--clr-accent);
+	padding: 2em;
+	margin: 0.5em;
+	border-radius: 1em;
+	width: 100%;
+}
+
+.info__title {
+	font-size: 1.8rem;
+	margin: 0.2em 0;
+}
+
+.info__value {
+	font-size: 1.5rem;
+	margin-left: 0.2em;
+}
+</style>
