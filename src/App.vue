c41fc4f8f163173bbf26b379ad4ffa124a95a93a
wroveda <walter.roveda@posteo.it>
2022-07-25 14:52:35 +0200

    Add log system

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,24 @@
 import wretch from "wretch";
 import { ref } from "vue";
 import InfoCard from "./components/InfoCard.vue";
+import LogList from "./components/LogList.vue";
 
 const panel = ref({});
+const orderedLogs = ref([{}])
 
 wretch()
 	.url("https://ott-fogliata.github.io/vuejs-s2i-repository/solar-panels.json")
 	.get()
-	.json(json => {panel.value = json})
+	.json(json => {
+		panel.value = json
+		orderedLogs.value = panel
+			.value
+			.logs
+			.sort((a, b) => {
+				return new Date(b.date) - new Date(a.date)
+			})
+		})
+
 </script>
 
 <template>
@@ -20,6 +31,12 @@ wretch()
 		<InfoCard infoTitle="Today's Energy" :infoValue="panel['today-energy'] + ' kWh'" />
 		<InfoCard infoTitle="Left Time Energy" :infoValue="panel['left-time-energy'] + ' kWh'" />
 	</div>
+	<div class="log-container">
+		<h2>Logs</h2>
+		<ul>
+			<LogList v-for="(log, index) in orderedLogs" :key="index" :Log="log"/>
+		</ul>
+	</div>
 </template>
 
 <style>
@@ -28,11 +45,18 @@ wretch()
 	--clr-bg: #e4d9b4;
 	--clr-accent: #ffdd6e;
 	--clr-faded: #e4d295;
+	--clr-info: #eeddcc;
+	--clr-warn: #ffcb6b;
+	--clr-err: #fb4934;
 }
 
 body {
 	color: var(--clr-fg);
 	background-color: var(--clr-bg);
+}
+
+ul {
+	padding: 0 3em;
 }
 
 .info-container {
---
+++ src/components/LogList.vue
@@ -0,0 +1,52 @@
+<script setup>
+const props = defineProps({
+	Log: Object
+})
+</script>
+
+<template>
+	<div class="container">
+		<section :class="Log.type">{{ Log.date }}</section>
+		<section class="text">{{ Log.text }}</section>
+	</div>
+</template>
+
+<style scoped>
+.container {
+	display: flex;
+	margin-bottom: 1em;
+}
+
+.container:last-child {
+	margin-bottom: 0;
+}
+
+section {
+	padding: 0.7em;
+}
+
+.text {
+	flex: 1 1 1;
+	width: auto;
+	overflow: scroll;
+	background: var(--clr-faded);
+	border-radius: 0.5em;
+	margin-left: 0.5em;
+}
+
+/* Class names MUST correspond to a 'log.type' from the API */
+.info {
+	background-color: var(--clr-info);
+	border-radius: 0.5em;
+}
+
+.warning {
+	background-color: var(--clr-warn);
+	border-radius: 0.5em;
+}
+
+.error {
+	background-color: var(--clr-err);
+	border-radius: 0.5em;
+}
+</style>
